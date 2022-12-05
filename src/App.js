bd500cdbcc9a8115b78300f2f5bd8a7a7a52d8ca
Jeff Ripke <jripke74@gmail.com>
2022-12-04 16:48:40 -0800

    init commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import { Route, Switch } from 'react-router-dom';
+
+import AllQuotes from './pages/AllQuotes';
+import QuoteDetail from './pages/QuoteDetail';
+import NewQuotes from './pages/NewQuotes';
+
+function App() {
+  return (
+    <Switch>
+      <Route path="/quotes" exact>
+        <AllQuotes />
+      </Route>
+      <Route path="/quotes/:quoteId">
+        <QuoteDetail />
+      </Route>
+      <Route path="/new-quote">
+        <NewQuotes />
+      </Route>
+    </Switch>
+  );
+}
+
+export default App;
---
+++ src/pages/QuoteDetail.js
@@ -0,0 +1,5 @@
+const QuoteDetail = () => {
+  return <h1>Quotes Detail page</h1>
+};
+
+export default QuoteDetail;
