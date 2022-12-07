92718102c4e1cdfce9f636fb34d47bc8095fdc88
Jeff Ripke <jripke74@gmail.com>
2022-12-07 13:55:21 -0800

    add data output

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,15 +1,26 @@
 import { Fragment } from 'react';
 import { useParams, Route } from 'react-router-dom';
 
+import HighLightedQuote from '../components/quotes/HighlightedQuote';
 import Comments from '../components/comments/Comments';
+
+const DUMMY_QUOTES = [
+  { id: 'q1', author: 'Jeff', text: 'Learning React is fun!' },
+  { id: 'q2', author: 'Doug', text: 'Learning is fun' },
+];
 
 const QuoteDetail = () => {
   const params = useParams();
 
+  const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
+
+  if (!quote) {
+    return <p>No quote found!</p>;
+  }
+
   return (
     <Fragment>
-      <h1>Quotes Detail page</h1>
-      <p>{params.quoteId}</p>
+      <HighLightedQuote text={quote.text} author={quote.author} />
       <Route path={`/quotes/${params.quoteId}/comments`}>
         <Comments />
       </Route>
