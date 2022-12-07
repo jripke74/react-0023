4033d68f73721ababa7ed1a22fe2215577f1d8af
Jeff Ripke <jripke74@gmail.com>
2022-12-07 13:00:29 -0800

    add dummmy data to AllQuotes.js

--- src/pages/AllQuotes.js
+++ src/pages/AllQuotes.js
@@ -1,5 +1,12 @@
+import QuoteList from '../components/quotes/QuoteList';
+
+const DUMMY_QUOTES = [
+  { id: 'q1', author: 'Jeff', text: 'Learning React is fun!' },
+  { id: 'q2', author: 'Doug', text: 'Learning is fun'},
+]
+
 const AllQuotes = () => {
-  return <h1>All Quotes Page</h1>;
+  return <QuoteList quotes={DUMMY_QUOTES} />;
 };
 
 export default AllQuotes;
